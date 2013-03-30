f13e74f7dde6a47ea08bb4b10d5a2ee6881272b5
Zhen Kong <zkong@codeaurora.org>
2013-03-29 17:28:31 -0700

    qseecom: Fix issue with incomplete command exiting prematurely
    A process waiting on a signal can be awaken by any signals. We
    need to only continue processing when the condition of the
    wait event is met.
    
    Change-Id: Ib2102babbb505876f89b04399729e6ff5a475605
    Signed-off-by: Mona Hossain <mhossain@codeaurora.org>
    Signed-off-by: Zhen Kong <zkong@codeaurora.org>
    
    Conflicts:
    	drivers/misc/qseecom.c

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -563,10 +563,13 @@ static int __qseecom_process_incomplete_cmd(struct qseecom_dev_handle *data,
 					struct qseecom_command_scm_resp *resp)
 {
 	int ret = 0;
+	int rc = 0;
 	uint32_t lstnr;
 	unsigned long flags;
 	struct qseecom_client_listener_data_irsp send_data_rsp;
 	struct qseecom_registered_listener_list *ptr_svc = NULL;
+	sigset_t new_sigset;
+	sigset_t old_sigset;
 
 	#if 0//defined(CONFIG_LGE_BROADCAST_ONESEG) //QCT Patch SFS Fail
 	unsigned long end_time = 0;
@@ -600,7 +603,6 @@ static int __qseecom_process_incomplete_cmd(struct qseecom_dev_handle *data,
 		}
 		pr_debug("waking up rcv_req_wq and "
 				"waiting for send_resp_wq\n");
-
 		#if 0 //defined(CONFIG_LGE_BROADCAST_ONESEG) //QCT Patch SFS Fail
 		end_time = jiffies + msecs_to_jiffies(300);
 		
@@ -619,17 +621,23 @@ static int __qseecom_process_incomplete_cmd(struct qseecom_dev_handle *data,
 			pr_warning("Interrupted_patchQCT: send_cmd loop timeout wait\n");
 		} while (1);
 		#else
-		if (wait_event_freezable(qseecom.send_resp_wq,
-				__qseecom_listener_has_sent_rsp(data))) {
-			pr_warning("Interrupted: exiting send_cmd loop\n");
-			return -ERESTARTSYS;
-		}
-		#endif
-		
+		/* initialize the new signal mask with all signals*/
+		sigfillset(&new_sigset);
+		/* block all signals */
+		sigprocmask(SIG_SETMASK, &new_sigset, &old_sigset);
+
+		do {
+			if (!wait_event_freezable(qseecom.send_resp_wq,
+				__qseecom_listener_has_sent_rsp(data)))
+				break;
+		} while (1);
+
+		/* restore signal mask */
+		sigprocmask(SIG_SETMASK, &old_sigset, NULL);
 		if (data->abort) {
-			pr_err("Aborting listener service %d\n",
-				data->listener.id);
-			return -ENODEV;
+			pr_err("Abort clnt %d waiting on lstnr svc %d, ret %d",
+				data->client.app_id, lstnr, ret);
+			rc = -ENODEV;
 		}
 		qseecom.send_resp_flag = 0;
 		send_data_rsp.qsee_cmd_id = QSEOS_LISTENER_DATA_RSP_COMMAND;
