a00b1890c3093b8eb156d237a3399735b4d36715
Paul Reioux <reioux@gmail.com>
2014-11-20 11:24:57 -0600

    cpufreq_intellimm: intelliminmax cpu governor initial baseline commit
    intelliminmax (intellimm) governor is designed to work with the newer SOCs with fixed voltage rails (ie MSM8974+ SOCs).  It is designed to work within those
    fixed voltage ranges in order to maximize battery performance while creating a
    smooth UI operations.
    
    Copyright 2014 Paul Reioux <aka faux123>
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    WIP: Intellimm: remove unused code and change tblmap algorithm
    
    tblmap: add power-save frequencies for the first 5 entries
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    WIP: intellimm: more tweaks and table clean up
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    WIP: intellimm: more tweaks
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    WIP: intellimm: clean up derpy logic
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    WIP: intellimm: final clean up and algorithm tweaks
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    WIP: intellimm: finalizing part 2
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    WIP: intellimm: finalizing part 3
    
    add back burst for smoother UI
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    intellimm: fix div by zero derp
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>
    
    Conflicts:
    	include/linux/cpufreq.h
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -45,7 +45,7 @@ static inline int cpufreq_unregister_notifier(struct notifier_block *nb,
 						unsigned int list)
 {
 	return 0;
-}
+} 
 static inline void disable_cpufreq(void) { }
 #endif		/* CONFIG_CPU_FREQ */
 
@@ -389,6 +389,9 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
 extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
+extern struct cpufreq_governor cpufreq_gov_intellimm;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellimm)
 #endif
 
 
