e2ca08289c1c7cea8ecd528cf98f5e794246628c
RickRoll <victormlourenco@gmail.com>
2015-02-15 12:13:15 -0200

    Add buildscript

---
+++ build.sh
@@ -0,0 +1,19 @@
+if [ $# -gt 0 ]; then
+echo $1 > .version
+fi
+ 
+make ARCH=arm CROSS_COMPILE=/home/victor/arm-cortex_a15-linux-gnueabihf-linaro_4.9.3-2015.01/bin/arm-cortex_a15-linux-gnueabihf- zImage -j6
+ 
+cp arch/arm/boot/zImage ~/anykernel/
+ 
+cd ~/anykernel/
+ 
+zipfile="RedKernel-rXXX.zip"
+echo "making zip file"
+cp zImage zip/kernel/
+ 
+cd zip/
+rm -f *.zip
+zip -r $zipfile *
+rm -f /tmp/*.zip
+cp *.zip /tmp
