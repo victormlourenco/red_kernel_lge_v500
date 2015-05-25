601afe50fb2410d51872cf379a5e2be9f775ee80
VictorL <victor.lourenco@uniriotec.br>
2015-05-25 10:31:53 -0300

    cpufreq: Add hellsactive governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -393,6 +393,9 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellimm)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HELLSACTIVE)
+extern struct cpufreq_governor cpufreq_gov_hellsactive;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_hellsactive)
 #endif
 
 
