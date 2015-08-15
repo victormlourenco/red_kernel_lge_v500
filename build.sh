4f9fb4ed271f2b617ed3de860ac99c5b87fe6812
Victor Lourenço <victormlourenco@gmail.com>
2015-08-15 10:11:09 -0300

    Update buildscript

--- build.sh
+++ build.sh
@@ -2,18 +2,18 @@ if [ $# -gt 0 ]; then
 echo $1 > .version
 fi
  
-make ARCH=arm CROSS_COMPILE=/home/victor/uber/scripts/out/arm-linux-androideabi-4.9-cortex-a15/bin/arm-linux-androideabi- zImage -j6
+make ARCH=arm CROSS_COMPILE=/home/victor/UBERTC/out/arm-linux-androideabi-4.9-cortex-a15/bin/arm-linux-androideabi- zImage -j6
  
-cp arch/arm/boot/zImage ~/anykernel-awifi-dev/
+cp arch/arm/boot/zImage ~/anykernel_awifi/
  
-cd ~/anykernel-awifi-dev/
+cd ~/anykernel_awifi/
  
-zipfile="RedKernel-rXXX-awifi-dev.zip"
+zipfile="RedKernel-rXXX-awifi.zip"
 echo "making zip file"
 cp zImage zip/kernel/
  
 cd zip/
 rm -f *.zip
 zip -r $zipfile *
-rm -f /tmp/*.zip
+rm -f /kernel/*.zip
 cp *.zip /tmp
