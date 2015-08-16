548c85522c6c70231103a535c4e5973535445c78
Victor Lourenço <victormlourenco@gmail.com>
2015-08-16 20:59:08 -0300

    add buildscript

---
+++ build.sh
@@ -0,0 +1,19 @@
+if [ $# -gt 0 ]; then
+echo $1 > .version
+fi
+ 
+make ARCH=arm CROSS_COMPILE=/home/victor/UBERTC/out/arm-linux-androideabi-4.9-cortex-a15/bin/arm-linux-androideabi- zImage -j6
+ 
+cp arch/arm/boot/zImage ~/anykernel_awifi/
+ 
+cd ~/anykernel-awifi/
+ 
+zipfile="RedKernel-rXXX-awifi-dev.zip"
+echo "making zip file"
+cp zImage zip/kernel/
+ 
+cd zip/
+rm -f *.zip
+zip -r $zipfile *
+rm -f /tmp/*.zip
+cp *.zip /tmp
