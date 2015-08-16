01bd30add5fdc87c853fa1deb271502616c7237c
Victor Lourenço <victormlourenco@gmail.com>
2015-08-16 20:43:40 -0300

    Update buildscript

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ if [ $# -gt 0 ]; then
 echo $1 > .version
 fi
  
-make ARCH=arm CROSS_COMPILE=/home/victor/uber/scripts/out/arm-linux-androideabi-4.9-cortex-a15/bin/arm-linux-androideabi- zImage -j6
+make ARCH=arm CROSS_COMPILE=/home/victor/UBERTC/out/arm-linux-androideabi-4.9-cortex-a15/bin/arm-linux-androideabi- zImage -j6
  
 cp arch/arm/boot/zImage ~/anykernel/
  
